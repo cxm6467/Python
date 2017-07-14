47998409be4f896949e9ba4ad8ce0c1c599a26f3
Chris <cxm6467@gmail.com>
2017-07-13 20:30:30 -0400

    Tabs > Spaces
    Finally

--- Calendar.py
+++ Calendar.py
@@ -21,64 +21,64 @@ calendar = []
 ## Function for displaying welcome message
 def welcome():
 	## Welcome Message
-  print("Welcome, ") + USER_FIRST_NAME + "."
-  	
-  ## Wait a second before printing date/time
-  sleep(1)
-  
-  
-  print("Today is:  "+strftime("%A %B %d, %Y"))
-  print("It is now:  "+strftime("%I:%M:%S"))
-  
-  sleep(1)
-  
-  ## Ask user for input
-  
-  print("What would you like to do, "+USER_FIRST_NAME+"?")
+	print("Welcome, ") + USER_FIRST_NAME + "."
+		
+	## Wait a second before printing date/time
+	sleep(1)
+	
+	
+	print("Today is:  "+strftime("%A %B %d, %Y"))
+	print("It is now:  "+strftime("%I:%M:%S"))
+	
+	sleep(1)
+	
+	## Ask user for input
+	
+	print("What would you like to do, "+USER_FIRST_NAME+"?")
  
 def start_calendar():
-  ## Welcome User
+	## Welcome User
 	welcome()
-  
-  ## Only exit when the user wants to
+	
+	## Only exit when the user wants to
 	start = True
-  
-  	## Use above so user can control exit
-  	while(start):
+	
+	## Use above so user can control exit
+	while(start):
 		user_choice = raw_input("A to Add, U to Update, V to View, D to Delete, X to Exit:  ").upper()
-    
-      ## View Calendar
-      if user_choice == "V":
-		  ## Check if calendar is empty, if so let them know, if not print it
-      	if len(calendar.keys()) < 1:
-      		print("Sorry, your calendar is empty!")
-      	else:
-        	print(calendar)
+		
+		## View Calendar
+		if user_choice == "V":
+			## Check if calendar is empty, if so let them know, if not print it
+			if len(calendar.keys()) < 1:
+				print("Sorry, your calendar is empty!")
+			else:
+				print(calendar)
 
-    	##  update Calendar
-    	elif user_choice == "U":
-    		#Update Info
-    		date = raw_input("What date?  ")
-      	update = raw_input("Enter the update:  ")
-      
-      	#Update calendar
-      	calendar[date] = update
-      
-      	#Temporary Success
-      	print("Successful update")
-      	print(calendar)
-      
-    	## Add Info
-    	elif user_choice == "A":
-    		## Add Info
-      	event = raw_input("Enter Event:  ")
-   			date = raw_input("Enter Date (MM/DD/YYYY):  ")
-      		if len(date) > 10   or (  int(date[6:])  < int(strftime("%Y")) ):
+			##  update Calendar
+		elif user_choice == "U":
+			#Update Info
+			date = raw_input("What date?  ")
+			update = raw_input("Enter the update:  ")
+				
+			#Update calendar
+			calendar[date] = update
+			
+			#Temporary Success
+			print("Successful update")
+			print(calendar)
+			
+		## Add Info
+		elif user_choice == "A":
+			## Add Info
+			event = raw_input("Enter Event:  ")
+			date = raw_input("Enter Date (MM/DD/YYYY):  ")
+			if len(date) > 10   or (  int(date[6:])  < int(strftime("%Y")) ):
 
-        		print("You entered an invalid date!")
-          		
-          	try_again = raw_input("Try Again?  Y for Yes, N for No:  ").upper()
-            	if try_again == "Y":
-            		continue
-            	elif try_again == "N":
-                start = False
+				print("You entered an invalid date!")
+						
+				try_again = raw_input("Try Again?  Y for Yes, N for No:  ").upper()
+				if try_again == "Y":
+					continue
+				elif try_again == "N":
+					start = False
